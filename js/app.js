9a4b829cfb1ef90c9907ab8282fdab065314f29a
ahmedalzarka <ahmeh.alzarka@gmail.com>
2023-05-08 07:53:00 +0300

    fix the js problem

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-
     let requestBtn = document.getElementById("requestbtn")
     let oops = document.querySelector(".oops")
-    
-    requestBtn.addEventListener("click", () => {
+    let emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    requestBtn.addEventListener("click", e => {
+        e.preventDefault()
         let emailInput = document.getElementById("email").value
-        emailInput == "" ?  oops.classList.remove("hidden") : null
+        emailInput === "" ?  oops.innerHTML = "Oops! Please add your email" : !emailRegex.test(emailInput) ? oops.innerHtml = "Oops! Please check your email" : null
      })
 
